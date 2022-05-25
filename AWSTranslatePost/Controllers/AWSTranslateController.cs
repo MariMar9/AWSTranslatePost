71be7fb0549b89b7a376fd18b5be8e8d93572878
MariMar9 <garciacarrionmar@gmail.com>
2022-05-25 23:57:10 +0200

    commit

---
+++ AWSTranslatePost/Controllers/AWSTranslateController.cs
@@ -0,0 +1,62 @@
+﻿using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.Translate;
+using Amazon.Translate.Model;
+using AWSTranslatePost.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSTranslatePost.Controllers
+{
+    public class AWSTranslateController : Controller
+    {
+        AWSCredentials credenciales;
+
+        public AWSTranslateController()
+        {
+            /*CredentialProfileStoreChain perfil = new CredentialProfileStoreChain();
+            if (!perfil.TryGetAWSCredentials("Mar", out credenciales)){
+                throw new Exception("Credenciales no idsponibles.");
+            }*/
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string texto, string lenguaje)
+        {
+            if (texto == null || lenguaje == null)
+            {
+                ViewBag.Mensaje = "Rellene todos los campos.";
+            }
+            else
+            {
+                using(IAmazonTranslate client = new AmazonTranslateClient())
+                {
+                    TranslateTextRequest request = new TranslateTextRequest()
+                    {
+                        SourceLanguageCode = "auto",
+                        TargetLanguageCode = lenguaje,
+                        Text = texto
+                    };
+                    TranslateTextResponse resultado = await client.TranslateTextAsync(request);
+                    if (resultado.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.Mensaje = "Se ha producido un error en la traducción.";
+                    }
+                    else
+                    {
+                        ViewBag.Traduccion = resultado.TranslatedText;
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ AWSTranslatePost/Models/Translation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSTranslatePost.Models
+{
+    public class Translation
+    {
+        public string Texto { get; set; }
+        public string Lenguaje { get; set; }
+    }
+}
